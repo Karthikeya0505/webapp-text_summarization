ed3b30c127718fbb3b636622c97fec63b38d5d97
Karthikeya <122217264+Karthikeya0505@users.noreply.github.com>
2023-08-21 06:29:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask,render_template,request,redirect,url_for
 from transformers import pipeline
+from transformers import BartTokenizer, BartForConditionalGeneration
+import torch
 
 app = Flask(__name__)
 
@@ -11,16 +13,16 @@ def home():
 def summarize():
     if request.method == 'POST':
         data = request.form['data']
-        summarizer = pipeline("summarization")
-        max_length = int(request.form['max-length'])
-        result = summarizer(data,min_length = 5,max_length = max_length)
+        summarizer = pipeline("summarization",model="facebook/bart-large-cnn")
+        #max_length = int(request.form['max-length'])
+        result = summarizer(data,min_length = 5)
         try:
             res = result[0]['summary_text']
             length = len(res.split(" "))
         except KeyError as e:
             pass
 
-    return render_template('summary.html',result = res,length = length)
+    return render_template('index.html',result = res)
 
 if __name__ == '__main__':
     app.run(debug=True)
